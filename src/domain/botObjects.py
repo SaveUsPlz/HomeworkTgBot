d2714d47cb0a504f5b0c6c849320b9e6773b299e
Sava <sava.usoltsev@gmail.com>
2020-10-09 19:11:39 +0400

    Создание классов для Homework и Group

--- src/domain/botObjects.py
+++ src/domain/botObjects.py
@@ -1,11 +1,24 @@
 class User:
 
-    def __init__(self, id: str, name: str,) -> None:
+    def __init__(self, id: str, name: str, ) -> None:
         self.id = id
         self.name = name
         super().__init__()
 
 
 class Teacher(User):
-    def __init__(self, id: str, name: str,) -> None:
+    def __init__(self, id: str, name: str) -> None:
         super().__init__(id, name)
+
+
+class Hometask:
+    def __init__(self, id: int, group_id: str, ) -> None:
+        self.id = id
+        self.group_id = group_id
+        super().__init__()
+
+
+class Group:
+    def __init__(self, id: str, ) -> None:
+        self.id = id
+        super().__init__()
